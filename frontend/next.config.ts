d1088f31591c322d99275ccb4b7d817248ab4107
shub <shubhamkrishan999@gmail.com>
2026-09-09 13:36:01 +0530

    trailing slash fixup

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/frontend',
+  trailingSlash: true,
   /* config options here */
   async headers() {
     return [
